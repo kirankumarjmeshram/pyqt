891c0a28751b81f988c0190e73113eae6cf92cc5
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-11-02 18:47:52 +0530

    added labe and change font using QtGui

--- hello.py
+++ hello.py
@@ -1,8 +1,25 @@
 import PyQt5.QtWidgets as qtw
+import PyQt5.QtGui as qtg
 
 class MainWindow(qtw.QWidget):
     def __init__(self):
         super().__init__()
+        #Add a title
+        self.setWindowTitle("Hello World")
+
+        #Set layout
+        #vertical box layout QVBoxLayout 
+        # horizontal QHBoxLayout
+        self.setLayout(qtw.QVBoxLayout())
+
+        #  label
+        my_label = qtw.QLabel('Hello World')
+        
+        #change font size
+        my_label.setFont(qtg.QFont('Helvetica',24))
+
+        self.layout().addWidget(my_label)
+
 
         self.show()
 
