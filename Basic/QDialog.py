0b7ad184ca39e6107202f8b592c66f9e374bc198
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-11-08 12:11:13 +0530

    QDialog

---
+++ Basic/QDialog.py
@@ -0,0 +1,42 @@
+# dialog.py
+
+"""Dialog-style application."""
+# A dialog window is a stand-alone window that you can use as the main window for your application.
+
+import sys
+
+from PyQt6.QtWidgets import (
+    QApplication,
+    QDialog,
+    QDialogButtonBox,
+    QFormLayout,
+    QLineEdit,
+    QVBoxLayout,
+)
+
+class Window(QDialog):
+    def __init__(self):
+        super().__init__(parent=None)
+        self.setWindowTitle("QDialog")
+        dialogLayout = QVBoxLayout()
+        formLayout = QFormLayout()
+        formLayout.addRow("Name:", QLineEdit())
+        formLayout.addRow("Age:", QLineEdit())
+        formLayout.addRow("Job:", QLineEdit())
+        formLayout.addRow("Hobbies:", QLineEdit())
+        dialogLayout.addLayout(formLayout)
+        buttons = QDialogButtonBox()
+
+        #Dialog windows can also provide a return value and have default buttons, such as Ok and Cancel.
+        buttons.setStandardButtons(
+            QDialogButtonBox.StandardButton.Cancel
+            | QDialogButtonBox.StandardButton.Ok
+        )
+        dialogLayout.addWidget(buttons)
+        self.setLayout(dialogLayout)
+
+if __name__ == "__main__":
+    app = QApplication([])
+    window = Window()
+    window.show()
+    sys.exit(app.exec())
