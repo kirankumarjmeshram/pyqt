fd9a88e432873fe30bf78350a8c76e11ff2101e0
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-11-25 17:08:14 +0530

    login functionality

--- projects/School Management/school.py
+++ projects/School Management/school.py
@@ -10,6 +10,25 @@ class MainApp(QMainWindow, ui):
     def __init__(self):
         QMainWindow.__init__(self)
         self.setupUi(self)
+
+        # set 1st tab default
+        self.tabWidget.setCurrentIndex(0)
+        # hide visibility of other tabs
+        self.tabWidget.tabBar().setVisible(False)
+        # hide menu visibility
+        self.menubar.setVisible(False)
+        self.b01.clicked.connect(self.login)
+
+    def login(self):
+        un = self.tb01.text()
+        pw = self.tb02.text()
+        if(un == 'admin' and pw == 'admin'):
+            self.menubar.setVisible(True)
+            self.tabWidget.setCurrentIndex(1)
+        else:
+            QMessageBox.information(self,"School Management System","Invalid admin login details, Try again !")
+            self.l01.setText("Invalid admin login details, Try again !")
+
 def main():
     app = QApplication(sys.argv)
     window = MainApp()
