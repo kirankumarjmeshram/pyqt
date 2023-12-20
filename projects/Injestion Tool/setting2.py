a910facd12264d5067e81458ad94c6670f46b28c
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-12-20 18:27:05 +0530

    updating setting app, added values from json file

--- projects/Injestion Tool/setting2.py
+++ projects/Injestion Tool/setting2.py
@@ -1,4 +1,5 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLineEdit, QLabel, QPushButton
+import json
 
 class MyApp(QWidget):
     def __init__(self):
@@ -11,6 +12,22 @@ class MyApp(QWidget):
         layout = QVBoxLayout()
         self.setLayout(layout)
 
+        # DEFAULT VALUES getting from default_config.json
+        with open('default_config.json') as f:
+            config_val = json.load(f)
+        
+        default_mongo_port = config_val["default_mongo_port"]
+        default_mongo_ip = config_val["default_mongo_ip"]
+        default_database_name = config_val["default_database_name"]
+        default_backup_path = config_val["default_backup_path"]
+
+ 
+        # default_mongo_port = "127.0.0.1"
+        # default_mongo_ip = "27017"
+        # default_database_name = "GARUDA"
+        # default_backup_path = "/home/$user/GARUDA/"
+
+
         update_reset_layout = QHBoxLayout()
         reset_btn = QPushButton("RESET")
         update_btn = QPushButton("UPDATE")
@@ -21,7 +38,7 @@ class MyApp(QWidget):
         mongo_port_layout = QHBoxLayout()
         mongo_port_label = QLabel("MONGO PORT")
         mongo_port_label.setFixedWidth(150)
-        mongo_port_input = QLineEdit("127.0.0.1")
+        mongo_port_input = QLineEdit(default_mongo_port)
         mongo_port_btn = QPushButton("UPDATE")
         mongo_port_layout.addWidget(mongo_port_label)
         mongo_port_layout.addWidget(mongo_port_input)
@@ -31,7 +48,7 @@ class MyApp(QWidget):
         mongo_ip_layout = QHBoxLayout()
         mongo_ip_label = QLabel("MONGO IP")
         mongo_ip_label.setFixedWidth(150)
-        mongo_ip_input = QLineEdit("27017")
+        mongo_ip_input = QLineEdit(default_mongo_ip)
         mongo_ip_btn = QPushButton("UPDATE")
         mongo_ip_layout.addWidget(mongo_ip_label)
         mongo_ip_layout.addWidget(mongo_ip_input)
@@ -41,7 +58,7 @@ class MyApp(QWidget):
         database_name_layout = QHBoxLayout()
         database_name_label = QLabel("DATABASE NAME")
         database_name_label.setFixedWidth(150)
-        database_name_input = QLineEdit("GARUDA")
+        database_name_input = QLineEdit(default_database_name)
         database_name_btn = QPushButton("UPDATE")
         database_name_layout.addWidget(database_name_label)
         database_name_layout.addWidget(database_name_input)
@@ -51,7 +68,7 @@ class MyApp(QWidget):
         backup_path_layout = QHBoxLayout()
         backup_path_label = QLabel("BACKUP PATH")
         backup_path_label.setFixedWidth(150)
-        backup_path_input = QLineEdit("/home/$user/GARUDA/")
+        backup_path_input = QLineEdit(default_backup_path)
         backup_path_btn = QPushButton("UPDATE")
         backup_path_layout.addWidget(backup_path_label)
         backup_path_layout.addWidget(backup_path_input)
